6f26366e5c5b04f1f595dea9fbbbc50f6ab6b8a7
LucasRDS2007 <lucasramosdossantos07@gmail.com>
2026-07-22 20:42:24 -0300

    feat/adiciona_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,134 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: NossaAppBar(),
-        drawer: Drawer(),
+        drawer: Drawer(
+          width: 350,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.green,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Container(
+                        height: 96,
+                        width: 96,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green.shade50,
+                        ),
+                        child: Icon(
+                          Icons.person,
+                          size: 32,
+                          color: Color(0xff146C43),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      'Lucas Ramos dos Santos',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.hail, size: 36, color: Color(0xff146C43)),
+                    SizedBox(width: 25),
+                    Text(
+                      'Viajar pela Internet',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.push_pin, size: 36, color: Color(0xff146C43)),
+                    SizedBox(width: 25),
+                    Text(
+                      'Fixar a Página',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.share, size: 36, color: Color(0xff146C43)),
+                    SizedBox(width: 25),
+                    Text(
+                      'Compartilhar a Página',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.save, size: 36, color: Color(0xff146C43)),
+                    SizedBox(width: 25),
+                    Text(
+                      'Salvar a Página',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.settings, size: 36, color: Color(0xff146C43)),
+                    SizedBox(width: 25),
+                    Text(
+                      'Configurações',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -46,6 +173,7 @@ class MainApp extends StatelessWidget {
                     padding: EdgeInsets.symmetric(vertical: 12),
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(8),
                       color: Colors.white,
                     ),
                     child: Column(
@@ -75,6 +203,7 @@ class MainApp extends StatelessWidget {
                     padding: EdgeInsets.symmetric(vertical: 12),
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(8),
                       color: Colors.white,
                     ),
                     child: Column(
@@ -104,6 +233,7 @@ class MainApp extends StatelessWidget {
                     padding: EdgeInsets.symmetric(vertical: 12),
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(8),
                       color: Colors.white,
                     ),
                     child: Column(
@@ -160,9 +290,18 @@ class NossaAppBar extends StatelessWidget implements PreferredSizeWidget {
           fontWeight: FontWeight.w500,
         ),
       ),
+
       actions: [
-        Icon(Icons.menu, color: Colors.white, size: 24),
-        Padding(padding: EdgeInsets.only(right: 16)),
+        Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu, color: Colors.white, size: 24),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
       ],
       backgroundColor: Color(0xff146C43),
       foregroundColor: Colors.white,
