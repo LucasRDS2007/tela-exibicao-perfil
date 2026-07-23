3de2523049909fe34deaedfc448281c89cd83092
LucasRDS2007 <lucasramosdossantos07@gmail.com>
2026-07-23 20:39:39 -0300

    feat/ajustes_para_entrega

--- lib/main.dart
+++ lib/main.dart
@@ -268,6 +268,17 @@ class MainApp extends StatelessWidget {
             ],
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: colorGreen,
+          unselectedItemColor: colorGreen,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Configurações',
+            ),
+          ],
+        ),
       ),
     );
   }
